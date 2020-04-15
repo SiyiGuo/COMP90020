5fd4e8e74460b24b91e470b3c90d811d9684dc3e
Raymond Guo <1578942650@qq.com>
2020-04-15 15:43:53 +1000

    add get put operation

--- dynamodb/src/main/java/Node/Application.java
+++ dynamodb/src/main/java/Node/Application.java
@@ -3,6 +3,8 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,6 +14,16 @@ public class Application {
     @RequestMapping("/")
     public String home() {
         return "Hello Docker World";
+    }
+
+    @RequestMapping(value="/get", method=RequestMethod.GET)
+    public String get(@RequestParam String key) {
+        return key;
+    }
+
+    @RequestMapping(value="/put", method= RequestMethod.POST)
+    public String put(@RequestParam String key, @RequestParam String value) {
+        return key+value;
     }
 
     public static void main(String[] args) {
