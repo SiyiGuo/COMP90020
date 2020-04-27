69ba4a4c26c76c17cd643fde6a772ae6f0d3ef0f
Raymond Guo <1578942650@qq.com>
2020-04-27 20:15:20 +1000

    sytnax fix

--- distributeddb/src/main/java/raft/nodemodule/Node.java
+++ distributeddb/src/main/java/raft/nodemodule/Node.java
@@ -1,11 +1,12 @@
 package raft.nodemodule;
 
+import raft.LifeCycle;
 import raft.consensusmodule.*;
 import raft.logmodule.RaftLogModule;
 import raft.rpcmodule.RaftRpcServer;
 import raft.statemachinemodule.RaftStateMachine;
 
-public class RaftNode implements RaftLifeCycle {
+public class Node implements LifeCycle {
 
     private RaftRpcServer rpcServer;
     private RaftConsensus consensus;
@@ -17,20 +18,20 @@ public class RaftNode implements RaftLifeCycle {
     }
 
     public RequestVoteResult handleRequestVote(RequestVoteArgs args) {
-
+        return null;
     }
 
     public AppendEntriesResult handleAppendEntries(AppendEntriesArgs args) {
-
+        return null;
     }
 
     public ClientResponse handleClientRequest(ClientRequest req) {
-
+        return null;
     }
 
     // redirect to leader
     public ClientResponse redirect(ClientRequest req) {
-
+        return null;
     }
 
     @Override
