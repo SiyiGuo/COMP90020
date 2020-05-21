2bd31141af7911ee5d19ddf338dbc1801e01ee04
Mofan <li_mofan@qq.com>
2020-05-21 20:02:28 +1000

    read log file from distributeddb directory

--- pythonproxy/getNodeData.py
+++ pythonproxy/getNodeData.py
@@ -8,13 +8,14 @@ def DownloadMergedJson() -> str:
     result = {}
     logs = {}
     node_ids =[]
-    for f in glob.glob("history_*.json"):
-        node_ids.append(str(f).split('.')[0].split('_')[1])
+    for f in glob.glob("..\distributeddb\history_*.json"):
+        print(str(f))
+        node_ids.append(str(f).split('.')[2].split('_')[1])
 
     result["all_nodes"] = node_ids
 
-    for f in glob.glob("history_*.json"):
-        node_id = str(f).split('.')[0].split('_')[1]
+    for f in glob.glob("..\distributeddb\history_*.json"):
+        node_id = str(f).split('.')[2].split('_')[1]
         with open(f, "rb") as infile:
             result[node_id] = json.load(infile)
 
